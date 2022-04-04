1e92f5c3c445e44d7b23ee1e856e92d3065e5ab0
EpsilonPoint0 <noahguzik123@gmail.com>
2022-04-04 15:25:40 -0400

    Fixed info window layout on map and added access to current location. Bars don't populate on physical phone

--- app/src/main/java/com/example/itsfiveoclocksomewhere/MapsActivity.java
+++ app/src/main/java/com/example/itsfiveoclocksomewhere/MapsActivity.java
@@ -1,14 +1,22 @@
 package com.example.itsfiveoclocksomewhere;
 
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 import androidx.room.Room;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.itsfiveoclocksomewhere.databinding.ActivityMapsBinding;
 import com.google.android.gms.maps.CameraUpdate;
@@ -24,6 +32,13 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import com.lyft.lyftbutton.LyftButton;
 import com.lyft.lyftbutton.RideParams;
 import com.lyft.networking.ApiConfig;*/
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.MultiplePermissionsReport;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.DexterError;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.PermissionRequestErrorListener;
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
 import com.uber.sdk.android.core.UberSdk;
 import com.uber.sdk.android.rides.RideParameters;
 import com.uber.sdk.android.rides.RideRequestButton;
@@ -34,6 +49,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import timber.log.Timber;
+
 import com.uber.sdk.rides.client.ServerTokenSession;
 import com.uber.sdk.rides.client.error.ApiError;
 
@@ -43,37 +59,37 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private ActivityMapsBinding binding;
     private AppDatabase db;
     private List<Special> specialList;
+    TextView textView;
 
 
-    /*class CustomInfoWindowAdapter implements GoogleMap.InfoWindowAdapter {
+    class CustomInfoWindowAdapter implements GoogleMap.InfoWindowAdapter {
 
         // These are both viewgroups containing an ImageView with id "badge" and two TextViews with id
         // "title" and "snippet".
-        private final View mWindow;
+        private Activity context;
 
-        private final View mContents;
-
-        CustomInfoWindowAdapter(Context context) {
-            mWindow = getLayoutInflater().inflate(R.layout.map_marker, null);
-            mContents = getLayoutInflater().inflate(R.layout.map_marker, null);
+        public CustomInfoWindowAdapter(Activity context) {
+            this.context = context;
         }
 
         @Override
         public View getInfoWindow(Marker marker) {
-            render(marker, mWindow);
-            return mWindow;
+            return null;
         }
 
         @Override
         public View getInfoContents(Marker marker) {
-            render(marker, mWindow);
-            return mContents;
-        }
+            View view = context.getLayoutInflater().inflate(R.layout.map_marker, null);
 
-        private void render(Marker marker, View view) {
+            TextView tvTitle = (TextView) view.findViewById(R.id.tv_title);
+            TextView tvSubTitle = (TextView) view.findViewById(R.id.tv_subtitle);
 
+            tvTitle.setText(marker.getTitle());
+            tvSubTitle.setText(marker.getSnippet());
+
+            return view;
         }
-    }*/
+    }
 
 
     @Override
@@ -131,6 +147,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         requestButton.setCallback(callback);
         requestButton.loadRideInformation();
 
+
+
         /*ApiConfig apiConfig = new ApiConfig.Builder()
                 .setClientId("your_client_id")
                 .setClientToken("your_client_token")
@@ -153,6 +171,45 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         DBMethods.populateSpecials(db.specialDao());
 
         Timber.d("Map activity onCreate");
+
+        requestPermissions();
+    }
+
+    private void requestPermissions() {
+        // below line is use to request
+        // permission in the current activity.
+        Dexter.withContext(this)
+                // below line is use to request the number of
+                // permissions which are required in our app.
+                .withPermissions(Manifest.permission.ACCESS_COARSE_LOCATION,
+                        // below is the list of permissions
+                        Manifest.permission.ACCESS_FINE_LOCATION
+                        )
+                // after adding permissions we are
+                // calling an with listener method.
+                .withListener(new MultiplePermissionsListener() {
+                    @Override
+                    public void onPermissionsChecked(MultiplePermissionsReport multiplePermissionsReport) {
+                        Toast.makeText(MapsActivity.this, "All the permissions are granted..", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(List<PermissionRequest> list, PermissionToken permissionToken) {
+
+                    }
+                })
+                .withErrorListener(new PermissionRequestErrorListener() {
+            // this method is use to handle error
+            // in runtime permissions
+            @Override
+            public void onError(DexterError error) {
+                // we are displaying a toast message for error message.
+                Toast.makeText(getApplicationContext(), "Error occurred! ", Toast.LENGTH_SHORT).show();
+            }
+        })
+                // below line is use to run the permissions
+                // on same thread and to check the permissions
+                .onSameThread().check();
     }
 
     /**
@@ -180,7 +237,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             location.getLatitude();
             location.getLongitude();
 
-            p1 = new LatLng(location.getLatitude(), location.getLongitude() );
+            p1 = new LatLng(location.getLatitude(), location.getLongitude());
 
         } catch (Exception ex) {
 
@@ -190,9 +247,13 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         return p1;
     }
 
+    @SuppressLint("MissingPermission")
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+
+        mMap.setMyLocationEnabled(true);
+
 
         // Add a marker in Sydney and move the camera
         //LatLng columbus = new LatLng(40.005095,-83.027703);
@@ -201,7 +262,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         List<Bar> barList = db.barDao().getAllBars();
         specialList = db.specialDao().getAllSpecials();
-        List<Marker> markers= new ArrayList<Marker>();
+        List<Marker> markers = new ArrayList<Marker>();
         for (int i = 0; i < barList.size(); i++) {
             String snippet = "Special: " + specialList.get(i).specialInfo + "\nStart Time: " + specialList.get(i).startTime + "\nEnd Time: " + specialList.get(i).endTime;
             LatLng mark = getLocationFromAddress(MapsActivity.this, barList.get(i).address);
@@ -211,12 +272,24 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         LatLngBounds.Builder builder = new LatLngBounds.Builder();
         for (Marker marker : markers) {
             builder.include(marker.getPosition());
+
         }
         LatLngBounds bounds = builder.build();
         int padding = 0; // offset from edges of the map in pixels
         CameraUpdate cu = CameraUpdateFactory.newLatLngBounds(bounds, padding);
-        //mMap.setInfoWindowAdapter(new CustomInfoWindowAdapter(this));
+        mMap.setInfoWindowAdapter(new CustomInfoWindowAdapter(this));
+        mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+            @Override
+            public boolean onMarkerClick(Marker m) {
+
+                return false;
+            }
+        });
         googleMap.animateCamera(cu);
+
+        mMap.setMyLocationEnabled(true);
+        //mMap.setOnMyLocationButtonClickListener(this);
+        //mMap.setOnMyLocationClickListener(this);
         Timber.d("Map activity onStart");
     }
 }
--- app/src/main/java/com/example/itsfiveoclocksomewhere/PostLogin.java
+++ app/src/main/java/com/example/itsfiveoclocksomewhere/PostLogin.java
@@ -2,11 +2,22 @@ package com.example.itsfiveoclocksomewhere;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.itsfiveoclocksomewhere.ui.login.LoginActivity;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.MultiplePermissionsReport;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.DexterError;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.PermissionRequestErrorListener;
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
+
+import java.util.List;
 
 import timber.log.Timber;
 
@@ -16,6 +27,7 @@ public class PostLogin extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post_login);
+        requestPermissions();
     }
     /** Called when the user touches the button */
     public void sendMessageLogin(View view) {
@@ -27,5 +39,42 @@ public class PostLogin extends AppCompatActivity {
     public void sendMessageMap(View view) {
         Intent intent = new Intent(PostLogin.this, MapsActivity.class);
         startActivity(intent);
+    }
+
+    private void requestPermissions() {
+        // below line is use to request
+        // permission in the current activity.
+        Dexter.withContext(this)
+                // below line is use to request the number of
+                // permissions which are required in our app.
+                .withPermissions(Manifest.permission.ACCESS_COARSE_LOCATION,
+                        // below is the list of permissions
+                        Manifest.permission.ACCESS_FINE_LOCATION
+                )
+                // after adding permissions we are
+                // calling an with listener method.
+                .withListener(new MultiplePermissionsListener() {
+                    @Override
+                    public void onPermissionsChecked(MultiplePermissionsReport multiplePermissionsReport) {
+                        Toast.makeText(PostLogin.this, "All the permissions are granted..", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(List<PermissionRequest> list, PermissionToken permissionToken) {
+
+                    }
+                })
+                .withErrorListener(new PermissionRequestErrorListener() {
+                    // this method is use to handle error
+                    // in runtime permissions
+                    @Override
+                    public void onError(DexterError error) {
+                        // we are displaying a toast message for error message.
+                        Toast.makeText(getApplicationContext(), "Error occurred! ", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                // below line is use to run the permissions
+                // on same thread and to check the permissions
+                .onSameThread().check();
     }
 }
